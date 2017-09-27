25f92cb486c7df8bafcf23c1a11fead392ad8eb5
Francis Zorrilla <zorrillafrancis@hotmail.com>
2017-09-27 19:41:52 -0400

    Labol / Material

--- public/app/scripts/controllers/item.js
+++ public/app/scripts/controllers/item.js
@@ -8,39 +8,45 @@
  * Controller of the MobileCRMApp
  */
 angular.module('MobileCRMApp')
-.controller('ItemCtrl', function ($scope, item, companies, $location, $q, toaster, dialogs) {
-	$scope.item = item;
+	.controller('ItemCtrl', function ($scope, item, companies, $location, $q, toaster, dialogs) {
+		$scope.item = item;
 
-	$scope.save = function(){
-		$scope.item.save()
-		.then(function(){
-			toaster.success('The items was saved successfully');
-			$location.path('itemList');
-		})
-		.catch(function(error){
-			toaster.error(error.message);
-		});
-	};
+		$scope.list = [
+			{ item: 'Material' },
+			{ item: 'Labol' }
+		]
 
-	$scope.remove = function(){
-		var dlg = dialogs.confirm('Warning','Are you sure you want to remove?');
-		dlg.result.then(function(btn){
-			$scope.item.remove()
-			.then(function(){
-				toaster.success('The item was removed successfully');
-				$location.path('/itemList');
-			})
-			.catch(function(error){
-				toaster.error(error.message);
+		$scope.save = function () {
+			console.log($scope.item)
+			$scope.item.save()
+				.then(function () {
+					toaster.success('The items was saved successfully');
+					$location.path('itemList');
+				})
+				.catch(function (error) {
+					toaster.error(error.message);
+				});
+		};
+
+		$scope.remove = function () {
+			var dlg = dialogs.confirm('Warning', 'Are you sure you want to remove?');
+			dlg.result.then(function (btn) {
+				$scope.item.remove()
+					.then(function () {
+						toaster.success('The item was removed successfully');
+						$location.path('/itemList');
+					})
+					.catch(function (error) {
+						toaster.error(error.message);
+					});
 			});
-		});
-	};
+		};
 
-	$scope.assignCompanies = function(){
-		var itemCompanies = $scope.item.companies || [];
-		var dialog = dialogs.create('views/assignCompanies.html', 'AssignCompaniesCtrl', { companyList: companies.data, companies: itemCompanies });
-		dialog.result.then(function (res) {
-			$scope.item.companies = res.companies;
-		});
-	};
-});
+		$scope.assignCompanies = function () {
+			var itemCompanies = $scope.item.companies || [];
+			var dialog = dialogs.create('views/assignCompanies.html', 'AssignCompaniesCtrl', { companyList: companies.data, companies: itemCompanies });
+			dialog.result.then(function (res) {
+				$scope.item.companies = res.companies;
+			});
+		};
+	})
