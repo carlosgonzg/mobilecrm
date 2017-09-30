aad6e577d9ffa292fda3d77375166e8292afe03a
jdeveaux184 <j_deveaux184@hotmail.com>
2017-09-30 17:02:40 -0400

    Issue

--- public/app/scripts/directives/backbutton.js
+++ public/app/scripts/directives/backbutton.js
@@ -1,7 +1,7 @@
 angular.module('MobileCRMApp')
 .directive('backButton', function () {
   return {
-    template: '<button class="btn btn-default" ng-click="goBack()><i class="fa fa-reply" "></i>&nbsp;Go Back</button>',
+    template: '<button class="btn btn-default" ng-click="goBack()"><i class="fa fa-reply"></i>&nbsp;Go Back</button>',
     restrict: 'E',
     controller: function ($scope, $window) {
 		$scope.goBack = function () {
