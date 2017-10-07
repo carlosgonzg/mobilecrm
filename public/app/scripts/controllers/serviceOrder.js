26c0b39a12042dbfa698cd4cc78718f1959a0791
jdeveaux184 <j_deveaux184@hotmail.com>
2017-10-06 21:27:49 -0400

    maps

--- public/app/scripts/controllers/serviceOrder.js
+++ public/app/scripts/controllers/serviceOrder.js
@@ -114,6 +114,7 @@ angular.module('MobileCRMApp')
 						if ($scope.serviceOrder.siteAddressFrom && $scope.serviceOrder.siteAddress) {
 							 
 							 $scope.serviceOrder.siteAddress.distanceFrom = $scope.serviceOrder.siteAddressFrom.address1 && $scope.serviceOrder.siteAddress.address1 ? parseFloat((result * 0.00062137).toFixed(2)) : 0;
+							 initMap(p1coord,p2coord);
 							 $scope.$apply();
 
 						}
@@ -123,6 +124,85 @@ angular.module('MobileCRMApp')
 
 				return d.promise;
 	};
+
+	 function initMap(p1,p2) {
+			        var markerArray = [];
+
+			        // Instantiate a directions service.
+			        var directionsService = new google.maps.DirectionsService;
+
+			        // Create a map and center it on Manhattan.
+			        var map = new google.maps.Map(document.getElementById('map'), {
+			          zoom: 6,
+			          center: {lat: 28.39788010000001,
+						   	   lng: -81.33288979999998}
+			        });
+
+			        // Create a renderer for directions and bind it to the map.
+			        var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+
+			        // Instantiate an info window to hold step text.
+			        var stepDisplay = new google.maps.InfoWindow;
+
+			        // Display the route between the initial start and end selections.
+			        calculateAndDisplayRoute(
+			            directionsDisplay, directionsService, markerArray, stepDisplay, map);
+			        // Listen to change events from the start and end lists.
+			        
+			      }
+
+			      function calculateAndDisplayRoute(directionsDisplay, directionsService,
+			          markerArray, stepDisplay, map) {
+			        // First, remove any existing markers from the map.
+			        for (var i = 0; i < markerArray.length; i++) {
+			          markerArray[i].setMap(null);
+			        }
+
+			        // Retrieve the start and end locations and create a DirectionsRequest using
+			        // WALKING directions.
+			        var p1coord = new google.maps.LatLng($scope.serviceOrder.siteAddressFrom.latitude, $scope.serviceOrder.siteAddressFrom.longitude);
+					var p2coord = new google.maps.LatLng($scope.serviceOrder.siteAddress.latitude, $scope.serviceOrder.siteAddress.longitude);
+
+			        directionsService.route({
+			          origin: p1coord,
+			          destination: p2coord,
+			          travelMode: 'DRIVING'
+			        }, function(response, status) {
+			          // Route the directions and pass the response to a function to create
+			          // markers for each step.
+			          if (status === 'OK') {
+			            document.getElementById('warnings-panel').innerHTML =
+			                '<b>' + response.routes[0].warnings + '</b>';
+			            directionsDisplay.setDirections(response);
+			            // showSteps(response, markerArray, stepDisplay, map);
+			          } else {
+			            // window.alert('Directions request failed due to ' + status);
+			          }
+			        });
+			      }
+
+			      function showSteps(directionResult, markerArray, stepDisplay, map) {
+			        // For each step, place a marker, and add the text to the marker's infowindow.
+			        // Also attach the marker to an array so we can keep track of it and remove it
+			        // when calculating new routes.
+			        var myRoute = directionResult.routes[0].legs[0];
+			        for (var i = 0; i < myRoute.steps.length; i++) {
+			          var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+			          marker.setMap(map);
+			          marker.setPosition(myRoute.steps[i].start_location);
+			          attachInstructionText(
+			              stepDisplay, marker, myRoute.steps[i].instructions, map);
+			        }
+			      }
+
+			      function attachInstructionText(stepDisplay, marker, text, map) {
+			        google.maps.event.addListener(marker, 'click', function() {
+			          // Open an info window when the marker is clicked on, containing the text
+			          // of the step.
+			          stepDisplay.setContent(text);
+			          stepDisplay.open(map, marker);
+			        });
+			      }
 
 	$scope.wsClassItem = Item;
 	$scope.wsFilterItem =  $rootScope.userData.role._id != 1 && $rootScope.userData.role._id != 5 ? { 'companies._id': $rootScope.userData.company._id }: { };
@@ -381,4 +461,6 @@ angular.module('MobileCRMApp')
 
 	$scope.getBranches();
   	$scope.recalculate();
+
 });
+
--- public/app/scripts/directives/address.js
+++ public/app/scripts/directives/address.js
@@ -25,13 +25,9 @@ angular.module('MobileCRMApp')
 			// 	longitude: -81.33288979999998
 			// };
 
-			console.log($scope.ngModel)
-
 			var originPoint = {};
-			
-			console.log(originPoint)
 
-			var placeSearch, autocomplete;
+			var placeSearch, autocomplete, map, directionsService, directionsDisplay;
 
 			$scope.geolocate = function() {
 				if (navigator.geolocation) {
@@ -57,6 +53,85 @@ angular.module('MobileCRMApp')
 				(document.getElementById('address1')), {types: ['geocode']});
 				autocomplete.addListener('place_changed', fillInAddress);
 			}
+
+			 function initMap() {
+			        var markerArray = [];
+
+			        // Instantiate a directions service.
+			        var directionsService = new google.maps.DirectionsService;
+
+			        // Create a map and center it on Manhattan.
+			        var map = new google.maps.Map(document.getElementById('map'), {
+			          zoom: 6,
+			          center: {lat: 28.39788010000001,
+						   	   lng: -81.33288979999998}
+			        });
+
+			        // Create a renderer for directions and bind it to the map.
+			        var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+
+			        // Instantiate an info window to hold step text.
+			        var stepDisplay = new google.maps.InfoWindow;
+
+			        // Display the route between the initial start and end selections.
+			        calculateAndDisplayRoute(
+			            directionsDisplay, directionsService, markerArray, stepDisplay, map);
+			        // Listen to change events from the start and end lists.
+			        
+			      }
+
+			      function calculateAndDisplayRoute(directionsDisplay, directionsService,
+			          markerArray, stepDisplay, map) {
+			        // First, remove any existing markers from the map.
+			        for (var i = 0; i < markerArray.length; i++) {
+			          markerArray[i].setMap(null);
+			        }
+
+			        // Retrieve the start and end locations and create a DirectionsRequest using
+			        // WALKING directions.
+					var p1coord = new google.maps.LatLng(originPoint.latitude, originPoint.longitude);
+					var p2coord = new google.maps.LatLng($scope.ngModel.latitude, $scope.ngModel.longitude);
+
+			        directionsService.route({
+			          origin: p1coord,
+			          destination: p2coord,
+			          travelMode: 'WALKING'
+			        }, function(response, status) {
+			          // Route the directions and pass the response to a function to create
+			          // markers for each step.
+			          if (status === 'OK') {
+			            // document.getElementById('warnings-panel').innerHTML =
+			            //     '<b>' + response.routes[0].warnings + '</b>';
+			            directionsDisplay.setDirections(response);
+			            // showSteps(response, markerArray, stepDisplay, map);
+			          } else {
+			            // window.alert('Directions request failed due to ' + status);
+			          }
+			        });
+			      }
+
+			      function showSteps(directionResult, markerArray, stepDisplay, map) {
+			        // For each step, place a marker, and add the text to the marker's infowindow.
+			        // Also attach the marker to an array so we can keep track of it and remove it
+			        // when calculating new routes.
+			        var myRoute = directionResult.routes[0].legs[0];
+			        for (var i = 0; i < myRoute.steps.length; i++) {
+			          var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+			          marker.setMap(map);
+			          marker.setPosition(myRoute.steps[i].start_location);
+			          attachInstructionText(
+			              stepDisplay, marker, myRoute.steps[i].instructions, map);
+			        }
+			      }
+
+			      function attachInstructionText(stepDisplay, marker, text, map) {
+			        google.maps.event.addListener(marker, 'click', function() {
+			          // Open an info window when the marker is clicked on, containing the text
+			          // of the step.
+			          stepDisplay.setContent(text);
+			          stepDisplay.open(map, marker);
+			        });
+			      }
 
 			function fillInAddress() {
 				// Get the place details from the autocomplete object.
@@ -160,9 +235,12 @@ angular.module('MobileCRMApp')
 				$scope.ngModel.zipcode = data.zipcode.postal;
 				$scope.ngModel.latitude = place.geometry ? place.geometry.location.lat() : 0;
 				$scope.ngModel.longitude = place.geometry ? place.geometry.location.lng() : 0;
-				console.log(originPoint)
 				if (place.geometry) {
 					getDistance($scope.ngModel, originPoint);
+				}
+
+				if (originPoint) {
+					initMap();
 				}
 
 				$scope.$apply();
@@ -223,4 +301,6 @@ angular.module('MobileCRMApp')
 
     }
   };
+
+
 });
