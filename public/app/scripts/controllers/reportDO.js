8e885727d216cff097ef3a8498f07253c199a777
jdeveaux184 <j_deveaux184@hotmail.com>
2017-12-17 11:28:32 -0400

    report DO

--- public/app/scripts/controllers/reportDO.js
+++ public/app/scripts/controllers/reportDO.js
@@ -170,7 +170,7 @@ angular.module('MobileCRMApp')
 	$scope.getTotal = function(){
 		var total = 0;
 		for(var i = 0; i< $scope.deliveryOrders.length; i++){
-			total += $scope.deliveryOrders[i].getTotal($scope.deliveryOrder.siteAddress.distanceFrom, $scope.deliveryOrder.client.company);
+			total += $scope.deliveryOrders[i].getTotal($scope.deliveryOrders[i].siteAddress.distanceFrom, $scope.deliveryOrders[i].client.company);
 		}
 		return total;
 	};
