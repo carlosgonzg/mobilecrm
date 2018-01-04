1100552be1bb08baf95ce8825f898ebbcac1dd2d
Francis Zorrilla <zorrillafrancis@hotmail.com>
2018-01-04 08:32:36 -0400

    mail

--- api/mail.js
+++ api/mail.js
@@ -68,7 +68,7 @@ var sendMail = function (to, subject, body, isHtmlBody, attached, cc, cco, reply
 	else {
 		mailOptions.attachments = [];
 	}
-/* 	 smtpTransport.sendMail(mailOptions, function (error, response) {
+ 	 smtpTransport.sendMail(mailOptions, function (error, response) {
 		if (error) {
 			console.log(error);
 			deferred.reject(error);
@@ -76,7 +76,7 @@ var sendMail = function (to, subject, body, isHtmlBody, attached, cc, cco, reply
 			console.log('Message sent');
 			deferred.resolve(response);
 		}
-	});  */
+	});  
 	return deferred.promise; 
 };
 
