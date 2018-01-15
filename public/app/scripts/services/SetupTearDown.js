117610dc8010dcb2c230403dd5686af1ca819881
jdeveaux184 <j_deveaux184@hotmail.com>
2018-01-15 00:47:45 -0400

    Update SetupTearDown.js

--- public/app/scripts/services/SetupTearDown.js
+++ public/app/scripts/services/SetupTearDown.js
@@ -15,7 +15,7 @@ var a;
 		SetupTearDown.super.constructor.apply(this, arguments);
 		this.baseApiPath = "/api/SetupTearDown";
 		this.client = this.client || {};
-		this.invoiceNumber = this.invoiceNumber || '';
+		this.invoiceNumber = this.invoiceNumber || 'Pending Invoice';
 		this.tor = this.tor || '';
 		this.pono = this.pono || '';
 		this.unitno = this.unitno || '';
@@ -33,9 +33,9 @@ var a;
 		for(var i = 0; i < this.items.length; i++){
 			this.items[i] = new Item(this.items[i]);
 		}
-		if($rootScope.userData.role._id != 1 && $rootScope.userData.role._id != 5){
-			this.invoiceNumber = 'Pending Invoice';
-		}
+// 		if($rootScope.userData.role._id != 1 && $rootScope.userData.role._id != 5){
+// 			this.invoiceNumber = 'Pending Invoice';
+// 		}
 	}
 	var extend = function (child, parent) {
 		var key;
